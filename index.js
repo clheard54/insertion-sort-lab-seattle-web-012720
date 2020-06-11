8a856452da42517935e85098c02003f81a908839
clheard54 <clarion.heard@gmail.com>
2020-06-11 10:32:29 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(arr){
+    let min=arr[0];
+    let index = 0;
+    for (let i=0; i<arr.length; i++){
+        if (arr[i]<min){
+            min=arr[i]
+            index=i
+        }
+    }
+    arr.splice(index, 1)
+    return min
 }
 
 function insertionSort(array){
-
+    let sorted = []
+    while (array.length>0){
+        sorted.push(findMinAndRemove(array))
+    }
+    return sorted;
 }
